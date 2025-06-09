f58a454f4d333f1cf77441f8e650ee4e6877eafe
jeff <zjingxiang527@gmail.com>
2025-06-09 18:05:46 +0100

    add int m

--- git1/git1.1.c
+++ git1/git1.1.c
@@ -2,6 +2,9 @@
 int main (void){
     printf("hi, this is git1.1 in git1\n");
     printf("newline new\n");
+    int m=3;
    int x=4;
     
 }
+//对于merge conflict
+//发生情况:when two person change the same line in the repository
