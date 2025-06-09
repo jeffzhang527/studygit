0066f016fc865bb197b7564f184b5e4b7da9ee29
Jeff-zhang921 <ub24928@bristol.ac.uk>
2025-06-09 22:17:45 +0100

    add a comment

--- git1/git1.1.c
+++ git1/git1.1.c
@@ -25,6 +25,7 @@ return a+b;
 //git merge combine the branch from the test branch to the master branch
 //after merge, master branch will have the history changing that test branch have
 //merge two branch by first switch to main then, git merge <function branch>
+//branching are occur at local repository
 
 
 
