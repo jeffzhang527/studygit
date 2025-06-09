b14d168c2e23f4ca44253159a1bdedaccc4276ca
Jeff-zhang921 <ub24928@bristol.ac.uk>
2025-06-09 22:04:59 +0100

    add a function

--- git1/git1.1.c
+++ git1/git1.1.c
@@ -4,17 +4,27 @@ int main (void){
     printf("newline new\n");
  int m=5;
  int a=3;
- 
+
+
 }
+
+int sum(int a, int b)
+{
+return a+b;
+}
+
 //对于merge conflict
 //发生情况:when two person change the same line(area) in the repository
 //if changing is at different area it won't conflict
+
+
 //branching: different version of the code going in same repository 
 //git branch show all branch
 //git branch  <> create a new branch
 //switch between branch use git checkout <>
 //git merge combine the branch from the test branch to the master branch
-//after merge, master branch will have the history changing that test brach have
-//
+//after merge, master branch will have the history changing that test branch have
+//first check
+
 
 
