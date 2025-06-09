a1dc5ddd0ef626986d06f0820e518d376894d96f
jeff <zjingxiang527@gmail.com>
2025-06-09 17:24:29 +0100

    init

---
+++ git1/git1.1.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main (void){
+    printf("hi, this is git1.1 in git1\n");
+}
---
+++ git1/git1.2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    int j=0;
+    for(int i=0; i<3; i++){
+        j++;
+    }
+    
+    printf("hi this is git1.2\n");
+    printf("this is the number: %i\n", j);
+}
